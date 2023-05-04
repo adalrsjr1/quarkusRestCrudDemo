8f86505b62684e8a6e04d72cc7931b17d17a6e75
Adalberto Junior <adalrsjr1@gmail.com>
2023-05-03 21:51:35 -0400

    fix scripts to create jvm image

--- quarkus/build-quarkus-jvm.sh
+++ quarkus/build-quarkus-jvm.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 mvn clean package -Dno-native && docker build -f Dockerfile-quarkus-jvm -t rest-crud-quarkus-jvm .
+
+docker build -f Dockerfile-quarkus-jvm -t smarttuning/rest-crud-quarkus-jvm -t rest-crud-quarkus-native .
+
+docker push smarttuning/rest-crud-quarkus-jvm
+
